b3ab617a773aee30f7e604a0faf39b4158488af1
deepinder-gill <deepinderrgilll@gmail.com>
2026-02-04 03:24:34 +0530

    Update 33_twitter.py

--- 33_twitter.py
+++ 33_twitter.py
@@ -1,10 +1,11 @@
+import re
 url = input("URL:").strip()
 
-#username = url.replace("https://twitter.com", "") -- i.e. if we use url.replace but it will have ton of loopholes like url can have slash of question mark at end or user might give it without httos so we can also do
+#username = url.replace("https://twitter.com", "") -- i.e. if we use url.replace but it will have ton of loopholes like url can have slash of question mark at end or user might give it without httos et cetera so we can also do
 
 #username = url.removeprefix("https://twitter.com/")
 
 #or we can use re.sub
 
-re.sub(r"https://twitter.com/", "", url)
+username = re.sub(r"^(https?://)?(www\.)?twitter.com/", "", url)
 print(f"username: {username}")
