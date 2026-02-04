b0be8a0dd70b07171dedccafb3e6febbb90465f6
deepinder-gill <deepinderrgilll@gmail.com>
2026-02-05 01:10:18 +0530

    Create 36_student(dict).py

---
+++ 36_student(dict).py
@@ -0,0 +1,11 @@
+def main():
+  student = get_student()
+  print(f"{student['name']} from {student['classs']}")
+
+def get_student():
+  student = {}
+  student["name"] = input("name: ")
+  student["classs"] = input("house: ")
+  return student
+if __name__ == "__main__":
+  main()
