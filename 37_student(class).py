7804f7a2b440e5c3887b67c84e96366c807c5474
deepinder-gill <deepinderrgilll@gmail.com>
2026-02-05 01:27:20 +0530

    Update 37_student(class).py

--- 37_student(class).py
+++ 37_student(class).py
@@ -1,13 +1,18 @@
 class Student:
   ...
 
+#declared class adn remember it must be in capital
+
 def main():
   student = get_student()
   print(f"{student.name} from {student.classs}")
 
 def get_student():
   student = Student()
+
+#assigner that in variable
   student.name = input("name: ")
+#used this to take input and store it in variable
   student.classs = input("classs: ")
   return student
 if __name__ == "__main__":
