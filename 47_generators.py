d56e6bcee2e986ec08c754bd58d955ea7be2683b
deepinder-gill <deepinderrgilll@gmail.com>
2026-02-06 03:12:56 +0530

    Create 47_generators.py

---
+++ 47_generators.py
@@ -0,0 +1,12 @@
+def main():
+  n = int(input("whaats n?!"))
+  for i in range(n):
+    print("🦭" * i)
+
+def seattle(n):
+    return "🦭" * n
+
+if __name__ == "__main__" :
+    main()
+
+#mofo dont try 10000000 
