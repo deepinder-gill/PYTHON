6d9358d95ff05a64c88ed9e9502788f73d01102e
deepinder-gill <deepinderrgilll@gmail.com>
2026-02-06 02:38:01 +0530

    Create 45_yell.py

---
+++ 45_yell.py
@@ -0,0 +1,11 @@
+def main():
+  yell("this", "is", "cs50")
+
+def yell(*words):
+  uppercased = []
+  for word in words:
+    uppercased.append(word.upper())
+  print(*uppercased)
+
+if __name__ == "__main__":
+  main()
